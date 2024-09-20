2c0f80485e7d61ae31f999e1f6903124bec7b6f5
Segward <gustask@stud.ntnu.no>
2024-09-20 02:32:02 +0200

    Commit 4

--- External Detour/main.cpp
+++ External Detour/main.cpp
@@ -10,6 +10,7 @@ int main() {
     uintptr_t function3 =       0x1400014C3;                        // function3
     uintptr_t nop1 =            0x140001545;                        // nop address1
     uintptr_t nop2 =            0x14000154F;                        // nop address2
+    uintptr_t sleep =           0x140001518;                        // sleep address
 
     Memory memory("Target.exe");
     uintptr_t baseAddress = memory.getModuleBase("Target.exe");
@@ -21,7 +22,7 @@ int main() {
     uintptr_t returnAddress = patchAddress + 0x10;                  // return address to after patch
     uintptr_t nop1Address = baseAddress + nop1 - rebase;            // address1 of 5 bytes to nop
     uintptr_t nop2Address = baseAddress + nop2 - rebase;            // address2 of 5 bytes to nop
-
+    uintptr_t sleepAddress = baseAddress + sleep - rebase;          // sleep address
 
     unsigned char patchData[15] = {
         0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov rax, 0 ; index 2
@@ -47,11 +48,21 @@ int main() {
         0xFF, 0xE0,                                                 // jmp rax
     };
 
-    unsigned char nop1Data[5] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
-    unsigned char nop2Data[5] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
+    unsigned char nop1Data[5] = { 
+        0x90, 0x90, 0x90, 0x90, 0x90                                // nop
+    };
+    
+    unsigned char nop2Data[5] = { 
+        0x90, 0x90, 0x90, 0x90, 0x90                                // nop
+    };
+
+    unsigned char sleepData[7] = { 
+        0xC7, 0x45, 0xF8, 0x02, 0x00, 0x00, 0x00                    // mov dword ptr [rbp-8], 2
+    };
 
     BOOL writeNop1 = memory.writeEx(nop1Address, nop1Data, sizeof(nop1Data));
     BOOL writeNop2 = memory.writeEx(nop2Address, nop2Data, sizeof(nop2Data));
+    BOOL writeSleep = memory.writeEx(sleepAddress, sleepData, sizeof(sleepData));
 
     const char* szStr = "Goodbye, World!";
     LPVOID szStrAddress = memory.allocateEx(strlen(szStr));
